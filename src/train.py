d25516370b04ce0aba08aaf87509a4d0f3d73b9c
yusufakyol2597 <55831103+yusufakyol2597@users.noreply.github.com>
2020-12-08 18:39:57 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -4,9 +4,10 @@ from gmf import GMFEngine
 from mlp import MLPEngine
 from neumf import NeuMFEngine
 from data import SampleGenerator
+import sys
 
 gmf_config = {'alias': 'gmf_factor8neg4-implict',
-              'num_epoch': 200,
+              'num_epoch': 100,
               'batch_size': 1024,
               # 'optimizer': 'sgd',
               # 'sgd_lr': 1e-3,
@@ -17,75 +18,82 @@ gmf_config = {'alias': 'gmf_factor8neg4-implict',
               # 'rmsprop_momentum': 0,
               'optimizer': 'adam',
               'adam_lr': 1e-3,
-              'num_users': 6040,
-              'num_items': 3706,
+              'num_users': 5270,
+              'num_items': 14872,
               'latent_dim': 8,
               'num_negative': 4,
               'l2_regularization': 0, # 0.01
-              'use_cuda': True,
+              'use_cuda': False,
               'device_id': 0,
               'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
 
 mlp_config = {'alias': 'mlp_factor8neg4_bz256_166432168_pretrain_reg_0.0000001',
-              'num_epoch': 200,
+              'num_epoch': 100,
               'batch_size': 256,  # 1024,
               'optimizer': 'adam',
               'adam_lr': 1e-3,
-              'num_users': 6040,
-              'num_items': 3706,
+              'num_users': 5270,
+              'num_items': 14872,
               'latent_dim': 8,
-              'num_negative': 4,
+              'num_negative': 6,
               'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
               'l2_regularization': 0.0000001,  # MLP model is sensitive to hyper params
-              'use_cuda': True,
+              'use_cuda': False,
               'device_id': 7,
               'pretrain': True,
-              'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4_Epoch100_HR0.6391_NDCG0.2852.model'),
+              'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4-implict_Epoch99_HR1.0000_NDCG0.6184.model'),
               'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
 
 neumf_config = {'alias': 'pretrain_neumf_factor8neg4',
-                'num_epoch': 200,
+                'num_epoch': 100,
                 'batch_size': 1024,
                 'optimizer': 'adam',
                 'adam_lr': 1e-3,
-                'num_users': 6040,
-                'num_items': 3706,
+                'num_users': 5270,
+                'num_items': 14872,
                 'latent_dim_mf': 8,
                 'latent_dim_mlp': 8,
                 'num_negative': 4,
-                'layers': [16,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
+                'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
                 'l2_regularization': 0.01,
-                'use_cuda': True,
+                'use_cuda': False,
                 'device_id': 7,
                 'pretrain': True,
-                'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4_Epoch100_HR0.6391_NDCG0.2852.model'),
-                'pretrain_mlp': 'checkpoints/{}'.format('mlp_factor8neg4_Epoch100_HR0.5606_NDCG0.2463.model'),
+                'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4-implict_Epoch99_HR1.0000_NDCG0.6184.model'),
+                'pretrain_mlp': 'checkpoints/{}'.format('mlp_factor8neg4_bz256_166432168_pretrain_reg_0.0000001_Epoch99_HR1.0000_NDCG0.6846.model'),
                 'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'
                 }
 
 # Load Data
-ml1m_dir = 'data/ml-1m/ratings.dat'
-ml1m_rating = pd.read_csv(ml1m_dir, sep='::', header=None, names=['uid', 'mid', 'rating', 'timestamp'],  engine='python')
+ml1m_dir = 'data/ml-1m/clicked.dat'
+ml1m_rating = pd.read_csv(ml1m_dir, sep=':', header=None, names=['uid', 'mid', 'rating', 'timestamp', 'impressions', 'is_test'],  engine='python')
+
+#uniques = ml1m_rating[['uid']].drop_duplicates(keep=False)
+#ml1m_rating = ml1m_rating[~ml1m_rating.index.isin(uniques.index)]
 # Reindex
 user_id = ml1m_rating[['uid']].drop_duplicates().reindex()
 user_id['userId'] = np.arange(len(user_id))
 ml1m_rating = pd.merge(ml1m_rating, user_id, on=['uid'], how='left')
-item_id = ml1m_rating[['mid']].drop_duplicates()
+item_id = ml1m_rating[['mid']]
 item_id['itemId'] = np.arange(len(item_id))
 ml1m_rating = pd.merge(ml1m_rating, item_id, on=['mid'], how='left')
-ml1m_rating = ml1m_rating[['userId', 'itemId', 'rating', 'timestamp']]
+#item_id['itemId'] = item_id['mid'].apply(lambda x: int(x))
+#ml1m_rating = pd.merge(ml1m_rating, item_id, on=['mid'], how='left')
+#ml1m_rating = pd.merge(ml1m_rating, item_id, on=['mid'], how='left')
+ml1m_rating = ml1m_rating[['userId', 'itemId', 'rating', 'timestamp', 'impressions', 'is_test', 'mid']]
+#print(ml1m_rating)
 print('Range of userId is [{}, {}]'.format(ml1m_rating.userId.min(), ml1m_rating.userId.max()))
 print('Range of itemId is [{}, {}]'.format(ml1m_rating.itemId.min(), ml1m_rating.itemId.max()))
 # DataLoader for training
 sample_generator = SampleGenerator(ratings=ml1m_rating)
 evaluate_data = sample_generator.evaluate_data
-# Specify the exact model
-config = gmf_config
-engine = GMFEngine(config)
-# config = mlp_config
-# engine = MLPEngine(config)
-# config = neumf_config
-# engine = NeuMFEngine(config)
+#Specify the exact model
+#config = gmf_config
+#engine = GMFEngine(config)
+#config = mlp_config
+#engine = MLPEngine(config)
+config = neumf_config
+engine = NeuMFEngine(config)
 for epoch in range(config['num_epoch']):
     print('Epoch {} starts !'.format(epoch))
     print('-' * 80)
